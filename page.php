8d413be42822146493bea91c56b47b823cdcb265
Chris Deaton <dcdeaton@ncsu.edU>
2021-06-11 13:49:05 -0400

    remove extra container

--- page.php
+++ page.php
@@ -12,7 +12,6 @@
 
 get_header(); ?>
 
-<div class="col-md-7">
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
 
@@ -31,7 +30,6 @@ get_header(); ?>
 
         </main><!-- #main -->
     </div><!-- #primary -->
-</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
